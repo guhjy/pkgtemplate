618aeafd32f0780969953cebec25a44945bd9d9b
cchoirat <cchoirat@gmail.com>
2015-10-25 12:52:57 -0400

    Changed to manual example

--- R/linreg.R
+++ R/linreg.R
@@ -5,7 +5,7 @@
 #' @param y response vector (1 x n)
 #' @param X covariate matrix (p x n) with no intercept
 #'
-#' @return A (p x 1) matrix of estimated parameters
+#' @return A list with 4 elements: coefficients, vcov, sigma, df
 #'
 #' @examples
 #' data(mtcars)
@@ -15,8 +15,21 @@
 #'
 #' @export
 #'
-linreg <- function(y, X) {
-  X <- cbind(1, X) # add intercept
-  beta_hat <- solve(t(X) %*% X) %*% t(X) %*% y
-  return(beta_hat)
+linreg <- function(x, y) {
+  ## compute QR-decomposition of x
+  qx <- qr(x)
+  ## compute (x’x)^(-1) x’y
+  coef <- solve.qr(qx, y)
+  ## degrees of freedom and standard deviation of residuals
+  df <- nrow(x) - ncol(x)
+  sigma2 <- sum((y - x %*% coef) ^ 2) / df
+  ## compute sigma^2 * (x’x)^-1
+  vcov <- sigma2 * chol2inv(qx$qr)
+  colnames(vcov) <- rownames(vcov) <- colnames(x)
+  list(
+    coefficients = coef,
+    vcov = vcov,
+    sigma = sqrt(sigma2),
+    df = df
+  )
 }
