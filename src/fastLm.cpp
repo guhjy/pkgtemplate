ccc0e2db5baa071e72dcffc0426604c7375034e5
cchoirat <cchoirat@gmail.com>
2015-10-25 15:55:36 -0400

    Added Armadillo example

---
+++ src/fastLm.cpp
@@ -0,0 +1,20 @@
+// [[Rcpp::depends(RcppArmadillo)]]
+
+#include <RcppArmadillo.h>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+List fastLm(const arma::vec & y, const arma::mat & X) {
+
+  int n = X.n_rows, k = X.n_cols;
+
+  arma::colvec coef = arma::solve(X, y);
+  arma::colvec resid = y - X*coef;
+
+  double sig2 = arma::as_scalar(arma::trans(resid)*resid/(n-k));
+  arma::colvec stderrest =
+    arma::sqrt(sig2 * arma::diagvec( arma::inv(arma::trans(X)*X)) );
+
+  return List::create(Named("coefficients") = coef,
+                      Named("stderr")       = stderrest);
+}
